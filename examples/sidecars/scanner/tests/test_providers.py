0a1d8bc8ec62a0f955ec1813e0575cbb0d6bcb87
kaitobui25 <nothink1320@gmail.com>
2026-08-08 21:56:32 +0900

    test FiinQuant universe mapping

--- examples/sidecars/scanner/tests/test_providers.py
+++ examples/sidecars/scanner/tests/test_providers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import asyncio
 import sys
 import unittest
 from pathlib import Path
@@ -23,6 +24,20 @@ class _MethodWrapper:
         return self._data
 
 
+class _TickerClient:
+    def __init__(self):
+        self.calls = []
+
+    def TickerList(self, **kwargs):
+        self.calls.append(kwargs)
+        payloads = {
+            'VNINDEX': ['VIC', 'FPT'],
+            'HNXIndex': [{'ticker': 'SHS', 'name': 'Sai Gon Ha Noi Securities'}],
+            'UpcomIndex': _DataWrapper([{'Ticker': 'MCH'}]),
+        }
+        return payloads[kwargs['ticker']]
+
+
 class ProviderTests(unittest.TestCase):
     def test_fiinquant_records_accept_data_wrapper(self):
         records = FiinQuantProvider._raw_records(_DataWrapper([
@@ -36,6 +51,34 @@ class ProviderTests(unittest.TestCase):
             {'Ticker': 'VHM', 'Close': 90},
         ]))
         self.assertEqual(records[0]['Ticker'], 'VHM')
+
+    def test_fiinquant_universes_use_sdk_index_names_and_keep_exchange(self):
+        client = _TickerClient()
+        provider = FiinQuantProvider('user', 'password')
+        provider._client = client
+
+        instruments = asyncio.run(provider.list_instruments(('HOSE', 'HNX', 'UPCOM')))
+
+        self.assertEqual(client.calls, [
+            {'ticker': 'VNINDEX'},
+            {'ticker': 'HNXIndex'},
+            {'ticker': 'UpcomIndex'},
+        ])
+        exchanges = {item.symbol: item.exchange for item in instruments}
+        self.assertEqual(exchanges, {
+            'FPT': 'HOSE',
+            'MCH': 'UPCOM',
+            'SHS': 'HNX',
+            'VIC': 'HOSE',
+        })
+
+    def test_fiinquant_rejects_unknown_universe(self):
+        client = _TickerClient()
+        provider = FiinQuantProvider('user', 'password')
+        provider._client = client
+
+        with self.assertRaisesRegex(ValueError, 'unsupported FiinQuant universe'):
+            asyncio.run(provider.list_instruments(('UNKNOWN',)))
 
 
 if __name__ == '__main__':
